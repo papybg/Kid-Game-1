84b2e6df751e3131893dcafb9d56aea2b3bc31b3
Temporary User <temp@example.com>
2025-11-18 21:04:28 +0200

    build(client): add resolve.alias '@' -> ./src to fix import resolution

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			// Allow imports like '@/utils/image-helpers' -> ./src/utils/image-helpers
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
 });
 
